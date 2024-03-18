b382e899541def90930722ea23fddcfb0996b3b7
Guthen <114919245+arkaht@users.noreply.github.com>
2024-03-18 22:02:56 +0100

    [wip] custom Cheat Manager

---
+++ Source/MetroXHunter/Cheats/CheatFunction.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "CheatFunction.generated.h"
+
+class UMXHCheatManager;
+
+/**
+ * 
+ */
+UCLASS( Abstract, Blueprintable, BlueprintType )
+class METROXHUNTER_API UCheatFunction : public UObject
+{
+	GENERATED_BODY()
+	
+public:
+	void Init( UMXHCheatManager* CheatManager );
+
+	UFUNCTION( BlueprintNativeEvent )
+	void OnInit();
+	UFUNCTION( BlueprintNativeEvent, BlueprintCallable, Category="Cheat Function" )
+	void OnCheat();
+
+	UWorld* GetWorld() const override;
+
+public:
+	UPROPERTY( EditAnywhere, BlueprintReadWrite )
+	FText Name;
+	UPROPERTY( EditAnywhere, BlueprintReadWrite )
+	FText Category;
+
+	UPROPERTY( BlueprintReadOnly )
+	UMXHCheatManager* CheatManager;
+};
