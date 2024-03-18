906f624cb866dbd432c204905a72d180040398dc
RustyGithubb <98985426+RustyGithubb@users.noreply.github.com>
2024-03-18 12:43:56 +0100

    init

---
+++ Source/MetroXHunter/MetroXHunterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MetroXHunterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class METROXHUNTER_API AMetroXHunterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
