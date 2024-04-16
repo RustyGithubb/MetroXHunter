dac62aafd3641fb30e813dedae1e98d58643cea2
Arthur Cathelain <114919245+arkaht@users.noreply.github.com>
2024-04-16 16:02:23 +0200

    [refactor] CheatFunction & docs

--- Source/MetroXHunter/Cheats/MXHCheatFunction.cpp
+++ Source/MetroXHunter/Cheats/MXHCheatFunction.cpp
@@ -2,29 +2,41 @@
 
 #include "MXHCheatManager.h"
 
+/**
+ * Cheat Function
+ */
+
 void UMXHCheatFunction::Init( UMXHCheatManager* _CheatManager )
 {
 	CheatManager = _CheatManager;
 
 	OnInit();
 
-	//  If should start toggled, enable it right after initialization so it allows user to initialize first before activation
-	if ( IsTogglable && ShouldStartToggled )
+	// If should start toggled, enable it right after initialization so it allows user to 
+	// initialize first before activation
+	if ( bIsTogglable && bShouldStartToggled )
 	{
 		FMXHCheatFunctionActionParams Params {};
-		Params.IsToggled = true;
+		Params.bIsToggled = true;
 		Cheat( Params );
 	}
 }
 
 void UMXHCheatFunction::Cheat( FMXHCheatFunctionActionParams Params )
 {
-	if ( IsTogglable )
+	if ( bIsTogglable )
 	{
-		IsToggled = Params.IsToggled;
+		bIsToggled = Params.bIsToggled;
 	}
 
 	OnCheat();
+}
+
+void UMXHCheatFunction::Cheat()
+{
+	FMXHCheatFunctionActionParams Params {};
+	Params.bIsToggled = bIsToggled;
+	Cheat( Params );
 }
 
 void UMXHCheatFunction::OnInit_Implementation() {}
@@ -36,4 +48,26 @@ UWorld* UMXHCheatFunction::GetWorld() const
 	if ( !IsValid( CheatManager ) ) return nullptr;
 
 	return CheatManager->GetWorld();
+}
+
+/**
+ * Cheat Selection Function
+ */
+
+void UMXHCheatSelectionFunction::CycleToNext()
+{
+	//  Move index to the right and wrap it around the value names length
+	Index = ( Index + 1 ) % ValueNames.Num();
+
+	Cheat();
+}
+
+void UMXHCheatSelectionFunction::CycleToPrevious()
+{
+	//  Move index to the left and wrap it around the value names length
+	Index = Index - 1 < 0 
+		? ValueNames.Num() - 1 
+		: Index - 1;
+
+	Cheat();
 }
