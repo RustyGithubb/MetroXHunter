2e0e1103775067b0ae7dd3448e1f0a537435ec64
Paya Corentin <corentin.gamedev@gmail.com>
2024-09-05 15:56:19 +0200

    [Refactor] HUD

---
+++ Source/MetroXHunter/HUD/MainHUD.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "MainHUD.generated.h"
+
+class UMXHCheatManager;
+class APlayerController;
+enum class E_InteractionType : uint8;
+
+UINTERFACE(MinimalAPI)
+class UMainHUD : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class METROXHUNTER_API IMainHUD
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION( BlueprintCallable, BlueprintImplementableEvent, Category = "HUD|Cheat Menu" )
+	bool ShowHideCheatMenuVisibility(UMXHCheatManager* CheatManager, APlayerController* PlayerController, bool bNewVisibility, bool bShouldToggle);
+
+	UFUNCTION( BlueprintCallable, BlueprintImplementableEvent, Category = "HUD|Interaction|Prompts" )
+	void UpdatePrompts( E_InteractionType InteractionType);
+
+	UFUNCTION( BlueprintCallable, BlueprintImplementableEvent, Category = "HUD" )
+	bool ShowNewTextIndication(const FText& TextToShow);
+
+	UFUNCTION( BlueprintCallable, BlueprintImplementableEvent, Category = "HUD" )
+	bool UpdateAmmoCount();
+
+	UFUNCTION( BlueprintCallable, BlueprintImplementableEvent, Category = "HUD|Interaction|Locker" )
+	bool ShowLockerSkillCheck(bool bNewLockerVisibility);
+
+	UFUNCTION( BlueprintCallable, BlueprintImplementableEvent, Category = "HUD|Interaction|Locker" )
+	bool GiveInputToLockerSkillCheck(FVector2D MovementsInput);
+
+	UFUNCTION( BlueprintCallable, BlueprintImplementableEvent, Category = "HUD|Interaction|Locker" )
+	bool PressingLockerNow();
+};
