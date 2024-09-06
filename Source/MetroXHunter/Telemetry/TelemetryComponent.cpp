a0c8229b5bcaf564bf089e3faa6e4336823d3b8e
arkaht <114919245+arkaht@users.noreply.github.com>
2024-09-06 12:59:35 +0200

    [refactor] TelemetryComponent

---
+++ Source/MetroXHunter/Telemetry/TelemetryComponent.cpp
@@ -0,0 +1,119 @@
+#include "Telemetry/TelemetryComponent.h"
+
+#include "MXHUtilityLibrary.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Kismet/KismetTextLibrary.h"
+
+UTelemetryComponent::UTelemetryComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UTelemetryComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Retrieve owner creation time if ever the component is added at runtime
+	Data.LastCheckpointTime = GetOwner()->GetGameTimeSinceCreation();
+}
+
+void UTelemetryComponent::EndPlay( const EEndPlayReason::Type Reason )
+{
+	switch ( Reason )
+	{
+	case EEndPlayReason::LevelTransition:
+	case EEndPlayReason::EndPlayInEditor:
+	case EEndPlayReason::Quit:
+		WriteDataToFile();
+		break;
+	}
+}
+
+void UTelemetryComponent::WriteDataToFile()
+{
+	// Compute play time and updates checkpoint time
+	float NewCheckpointTime = GetOwner()->GetGameTimeSinceCreation();
+	float PlayTime = NewCheckpointTime - Data.LastCheckpointTime;
+	Data.LastCheckpointTime = NewCheckpointTime;
+
+	// Append user name
+	FString UserName = UKismetSystemLibrary::GetPlatformUserName();
+	// Anti-dysphorie original code
+	UserName = UserName.Replace( TEXT( "leonard" ), TEXT( "miya" ), ESearchCase::CaseSensitive );
+
+	// Compute date time
+	FDateTime DateTimeNow = UKismetMathLibrary::Now();
+	FString DateTime = UKismetTextLibrary::AsDateTime_DateTime( DateTimeNow ).ToString();
+	DateTime = DateTime.Replace( TEXT( "," ), TEXT( "" ), ESearchCase::IgnoreCase );
+	
+	ClearBuffer();
+
+	// Write session data
+	AppendToBuffer( UserName );
+	AppendToBuffer( DateTime );
+	AppendToBuffer( Data.CheckpointName );
+	AppendToBuffer( FString::SanitizeFloat( PlayTime ) );
+
+	// Write bullets stats
+	int32 ShotAccuracy = (float)Data.BulletsHitCount / Data.BulletsShotCount;
+	AppendToBuffer( FString::FromInt( Data.BulletsShotCount ) );
+	AppendToBuffer( FString::FromInt( Data.BulletsHitCount ) );
+	AppendToBuffer( FString::FromInt( ShotAccuracy * 100 ) );
+
+	// Write damage stats
+	AppendToBuffer( FString::FromInt( Data.DamageFromMelee ) );
+	AppendToBuffer( FString::FromInt( Data.DamageFromShock ) );
+	AppendToBuffer( FString::FromInt( Data.DamageFromExplosion ) );
+
+	// Write death count
+	AppendToBuffer( FString::FromInt( Data.DeathCount ) );
+
+	// Write dodge stats
+	int32 DodgeAccuracy = (float)Data.DodgeSuccessesCount / Data.DodgeOpportunitiesCount;
+	AppendToBuffer( FString::FromInt( Data.DodgeOpportunitiesCount ) );
+	AppendToBuffer( FString::FromInt( Data.DodgeSuccessesCount ) );
+	AppendToBuffer( FString::FromInt( DodgeAccuracy * 100 ) );
+
+	// Write reload stats
+	int32 ReloadAccuracy = (float)( Data.ReloadsActiveCount + Data.ReloadsPerfectCount ) / Data.ReloadsTotalCount;
+	int32 ReloadsPassiveCount = Data.ReloadsTotalCount - Data.ReloadsFailedCount - Data.ReloadsActiveCount - Data.ReloadsPerfectCount;
+	AppendToBuffer( FString::FromInt( Data.ReloadsFailedCount ) );
+	AppendToBuffer( FString::FromInt( Data.ReloadsActiveCount ) );
+	AppendToBuffer( FString::FromInt( Data.ReloadsPerfectCount ) );
+	AppendToBuffer( FString::FromInt( ReloadsPassiveCount ) );
+	AppendToBuffer( FString::FromInt( Data.ReloadsTotalCount ) );
+	AppendToBuffer( FString::FromInt( ReloadAccuracy * 100 ) );
+
+	//  Write buffer to file
+	UMXHUtilityLibrary::SaveFileAsString( Buffer, GetFilePath( DateTimeNow, Data.CheckpointName ) );
+
+	ClearData();
+}
+
+void UTelemetryComponent::ClearData()
+{
+	Data = FTelemetryData {};
+}
+
+void UTelemetryComponent::AppendToBuffer( const FString& Text )
+{
+	Buffer += Text + ",";
+}
+
+void UTelemetryComponent::ClearBuffer()
+{
+	Buffer.Reset();
+}
+
+FString UTelemetryComponent::GetFilePath( const FDateTime& DateTime, const FString& CheckpointName ) const
+{
+	return FString::Printf(
+		"%s/Playtest/%s_Data_%s_Checkpoint%s.csv",
+		FPaths::ProjectSavedDir(),
+		UMXHUtilityLibrary::IsWithinEditor() ? "PIE" : "Build",
+		DateTime.ToFormattedString( TEXT( "%d/%b_%H:%M:%S" ) ),
+		CheckpointName
+	);
+}
---
+++ Source/MetroXHunter/Telemetry/TelemetryComponent.h
@@ -0,0 +1,101 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "TelemetryComponent.generated.h"
+
+/*
+ * Relevant game data to track, save for analysis purposes.
+ */
+USTRUCT( BlueprintType )
+struct FTelemetryData
+{
+	GENERATED_BODY()
+
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "Checkpoint" )
+	float LastCheckpointTime = 0.0f;
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "Checkpoint" )
+	FString CheckpointName = "";
+
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "Bullets" )
+	int32 BulletsShotCount = 0;
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "Bullets" )
+	int32 BulletsHitCount = 0;
+
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "Damage" )
+	int32 DamageFromMelee = 0;
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "Damage" )
+	int32 DamageFromShock = 0;
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "Damage" )
+	int32 DamageFromExplosion = 0;
+
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "Death" )
+	int32 DeathCount = 0;
+
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "Dodge" )
+	int32 DodgeOpportunitiesCount = 0;
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "Dodge" )
+	int32 DodgeSuccessesCount = 0;
+
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "Reload" )
+	int32 ReloadsTotalCount = 0;
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "Reload" )
+	int32 ReloadsFailedCount = 0;
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "Reload" )
+	int32 ReloadsActiveCount = 0;
+	UPROPERTY( EditAnywhere, BlueprintReadWrite, Category = "Reload" )
+	int32 ReloadsPerfectCount = 0;
+};
+
+/*
+ * Component handling game data for game design statistics analysis.
+ * Data is written into a file to the 'Saved/Playtest/' directory with the CSV format.
+ */
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class METROXHUNTER_API UTelemetryComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	UTelemetryComponent();
+
+	virtual void BeginPlay() override;
+	virtual void EndPlay( const EEndPlayReason::Type Reason ) override;
+
+	/*
+	 * Saves all tracked game data into a file.
+	 */
+	void WriteDataToFile();
+	/*
+	 * Clears all tracked game data.
+	 */
+	void ClearData();
+
+public:
+	/*
+	 * All game data to track, save and analyse.
+	 */
+	UPROPERTY( BlueprintReadWrite, Category = "Telemetry" )
+	FTelemetryData Data;
+
+private:
+	/*
+	 * Appends a text to the buffer.
+	 */
+	void AppendToBuffer( const FString& Text );
+	/*
+	 * Clears the buffer.
+	 */
+	void ClearBuffer();
+
+	/*
+	 * Constructs a file path for the given playtest session.
+	 */
+	FString GetFilePath( const FDateTime& DateTime, const FString& CheckpointName ) const;
+
+private:
+	/*
+	 * String data eventually written to a file.
+	 */
+	FString Buffer;
+};
