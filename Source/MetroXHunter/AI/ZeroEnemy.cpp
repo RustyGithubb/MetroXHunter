5702fc4de99b1d63ba351a3b6d3a1cff84f43503
arkaht <114919245+arkaht@users.noreply.github.com>
2024-09-10 12:33:40 +0200

    [add] ZeroEnemy behavior tree and movement

--- Source/MetroXHunter/AI/ZeroEnemy.cpp
+++ Source/MetroXHunter/AI/ZeroEnemy.cpp
@@ -1,5 +1,9 @@
 #include "AI/ZeroEnemy.h"
 
+#include "MXHUtilityLibrary.h"
+
+#include "Components/ArrowComponent.h"
+
 AZeroEnemy::AZeroEnemy()
 {
 	PrimaryActorTick.bCanEverTick = true;
@@ -11,9 +15,20 @@ AZeroEnemy::AZeroEnemy()
 void AZeroEnemy::BeginPlay()
 {
 	Super::BeginPlay();
+
+	RetrieveReferences();
 }
 	
 void AZeroEnemy::Tick( float DeltaTime )
 {
 	Super::Tick( DeltaTime );
+}
+
+void AZeroEnemy::RetrieveReferences()
+{
+	verifyf( IsValid( Data ), TEXT( "%s doesn't reference a DataAsset" ), *GetName() );
+
+	UMXHUtilityLibrary::PrintMessage( TEXT( "Hey!" ) );
+	auto BulbSpots = UMXHUtilityLibrary::GetComponentsOfActorByTag<UArrowComponent>( this, Data->BulbSpotTag );
+	auto BodyParts = UMXHUtilityLibrary::GetComponentsOfActorByTag<UStaticMeshComponent>( this, Data->BodyPartTag );
 }
