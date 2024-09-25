ddb738d7642b1ca345b3997ad9d1025dbef7403c
Benoit <98055777+Noxtibe@users.noreply.github.com>
2024-09-25 17:06:00 +0200

    [clean] Dlete useless Gun interface

--- Source/MetroXHunter/Gun/GunCommunication.cpp
+++
@@ -1,6 +0,0 @@
-// /* * Implemented by Firstname Lastname (Nickname) */
-
-
-#include "Gun/GunCommunication.h"
-
-// Add default functionality here for any IGunCommunication functions that are not pure virtual.
